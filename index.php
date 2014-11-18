e3f87f709e93c5843c771a24855671603fd6f386
Jonatas Reis <studioi9ideias@hotmail.com>
2014-11-18 18:35:44 -0200

    Criado index e view

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="UTF-8">
+	<title>Curso Git - compartilhado</title>
+</head>
+<body>
+	<h1>Curso Git - compartilhado</h1>
+</body>
+</html>
